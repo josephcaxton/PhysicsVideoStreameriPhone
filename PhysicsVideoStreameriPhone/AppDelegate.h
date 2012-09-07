f8727ce7fc341bf927a457d3e822c77324e8d02e
Joseph caxton-Idowu <Joseph@unknown-d8:30:62:54:ef:a0.home>
2012-09-07 12:44:17 +0100

    Initial Commit

---
+++ PhysicsVideoStreameriPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhysicsVideoStreameriPhone
+//
+//  Created by Joseph caxton-Idowu on 07/09/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ PhysicsVideoStreameriPhone/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  PhysicsVideoStreameriPhone
+//
+//  Created by Joseph caxton-Idowu on 07/09/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
