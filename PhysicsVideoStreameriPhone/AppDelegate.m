4cc5c1f4fb30953c1d06a3b6fae42715c24d4f09
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2013-03-21 11:21:27 +0000

    minor

--- PhysicsVideoStreameriPhone/AppDelegate.m
+++ PhysicsVideoStreameriPhone/AppDelegate.m
@@ -476,7 +476,7 @@ static NSString* const kAnalyticsAccountId = @"UA-34413575-1";
 //Return True is file does not exist in device -- so download from server;
 // Return True if the file exist but version is defferent--- So we need to download the file;
 // Return false if the file is same version --- so don't download;
--(BOOL)downloadfileifUpdated:(NSString*)urLString location:(NSString*)LocalFileLocation; {
+-(BOOL)downloadfileifUpdated:(NSString*)urLString location:(NSString*)LocalFileLocation {
     
     //DLog(@"Downloading HTTP header from: %@", urlString);
     
