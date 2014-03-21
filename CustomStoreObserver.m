90d31ae6dffa9035835be05f70555b260d5b60c2
josephcaxton <Josephcaxtonidowu@yahoo.com>
2014-03-21 09:30:06 +0000

    UIView fix

--- CustomStoreObserver.m
+++ CustomStoreObserver.m
@@ -207,7 +207,7 @@
     }
     
     else {
-        return @"Error";
+        return @"0";
     }
     
         
@@ -326,7 +326,7 @@
         if (buttonIndex == 1)
         {
             // Update Username and password in Database
-            for (UIView* view in actionSheet.subviews)
+            /*for (UIView* view in actionSheet.subviews)
             {
                 if ([view isKindOfClass:[UITextField class]])
                 {
@@ -340,11 +340,11 @@
                     {
                         Password = [[NSString alloc]initWithString:textField.text == nil ? @"" : textField.text];
                         [textField resignFirstResponder];
-                    }
+                    } */
                     
                    // break;
-                }
-            }
+            EmailAddress = [actionSheet textFieldAtIndex:0].text;
+            Password = [actionSheet textFieldAtIndex:1].text;
 
             
             
